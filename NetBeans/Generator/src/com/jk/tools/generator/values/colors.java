333467b97081e698aa4a041fa9fa92d56aba4d8a
jonggwon.kim <jg1052.kim@hanwha.com>
2018-06-27 19:57:11 +0900

    Add values for access public

---
+++ NetBeans/Generator/src/com/jk/tools/generator/values/colors.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jk.tools.generator.values;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author jonggwonkim
+ */
+public class colors {
+
+    public static final Color ORANGE = new Color(242,155,33);
+}
