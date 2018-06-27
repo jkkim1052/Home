fa1b754e88cea027dc5056f76bb86a4421a70258
jonggwon.kim <jg1052.kim@hanwha.com>
2018-06-27 22:03:57 +0900

    Change resource location and Add OS Type Select icon

---
+++ NetBeans/Generator/src/res/values/colors.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package res.values;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author jonggwonkim
+ */
+public class colors {
+
+    public static final Color WHITE = new Color(255,255,255);
+    public static final Color ORANGE = new Color(242,155,33);
+    
+    public static final Color TitleColor = WHITE;
+//    public static final Color AppName = new Color(255,153,102);
+    public static final Color AppName = WHITE;
+    public static final Color AppNameBG = ORANGE;
+    
+    public static final Color OSTypeBG = ORANGE;
+    public static final Color OSButtonText = WHITE;
+    public static final Color OSButtonBG = ORANGE;
+    
+    
+    public static final Color LanguageBG = new Color(243,243,243);
+}
---
+++ NetBeans/Generator/src/res/values/dimens.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package res.values;
+
+import java.awt.Dimension;
+
+/**
+ *
+ * @author jonggwonkim
+ */
+public class dimens {
+    public static final Dimension mainPanel = new Dimension(960, 540);
+    public static final Dimension fileopenPanel = new Dimension(960, 40);
+    public static final Dimension ostypePanel = new Dimension(200, 500);
+    public static final Dimension langPanel = new Dimension(760, 500);
+    public static final Dimension emptyPanel = new Dimension(50, 100);
+    
+    
+    /**
+     * File Open Panel View Size
+     */
+    public static final Dimension appName = new Dimension(200, 50);
+    
+    
+    /**
+     * OS Type select Panel View Size
+     */
+    public static final Dimension btnOSType = new Dimension(200, 50);
+    
+    
+    
+    
+    /**
+     * Define int Size
+     */
+    public static final int titleAppName = 30;
+}
